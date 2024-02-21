bdcc7bcca4c0b4deaeae699ed98feff00571179a
Jean-Aime-2023 <124204227+Jean-Aime-2023@users.noreply.github.com>
2024-02-21 21:42:23 +0200

    changes

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const Button = ({action}) => {
+const Button = ({ action }) => {
   return (
     <div>
-        <button className="bg-[#5547D7] hover:bg-[#7164e2] text-white text-md rounded-md flex max-md:text-sm items-center text-center px-11 py-2 justify-center">
-            {action}
-        </button>
+      <button className="bg-[#5547D7] hover:bg-[#7164e2] text-white text-md rounded-md flex max-md:text-sm items-center text-center px-11 py-2 justify-center">
+        {action}
+      </button>
     </div>
   )
 }
