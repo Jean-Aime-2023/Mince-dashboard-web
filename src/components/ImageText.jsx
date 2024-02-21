7f81d2a92eb314929bfd4bbb584cd71f8b9427c0
Jean-Aime-2023 <124204227+Jean-Aime-2023@users.noreply.github.com>
2024-02-21 21:42:50 +0200

    updates

--- src/components/ImageText.jsx
+++ src/components/ImageText.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-const ImageText = ({img,name}) => {
+const ImageText = ({ img, name }) => {
   return (
     <div className='flex flex-row gap-5 items-center'>
-        <div><img src={img} alt="" className='h-10 w-10 rounded-full' /></div>
-        <p>{name}</p> 
+      <div><img src={img} alt="" className='h-10 w-10 rounded-full' /></div>
+      <p>{name}</p>
     </div>
   )
 }
