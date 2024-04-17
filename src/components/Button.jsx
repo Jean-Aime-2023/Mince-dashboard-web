9c79f6ba1f5a197e64b70ff5187fa3703dc56b62
Jean-Aime-2023 <124204227+Jean-Aime-2023@users.noreply.github.com>
2024-04-17 10:31:05 +0200

    Deals page

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,14 +1,25 @@
-import React from 'react'
+import React from 'react';
 
-const Button = ({ action,icon }) => {
+const Button = ({ action, icon, bg, hoverBg }) => {
+  const buttonStyle = {
+    backgroundColor: bg,
+    transition: 'background-color 0.3s ease',
+    '&:hover': {
+      backgroundColor: hoverBg,
+    },
+  };
+
   return (
     <div>
-      <button className="bg-[#5547D7] hover:bg-[#7164e2] text-white text-md rounded-md flex max-md:text-sm items-center text-center px-11 py-2 justify-center flex-row gap-2">
+      <button
+        style={buttonStyle}
+        className="text-white text-md rounded-md flex max-md:text-sm items-center text-center px-11 py-2 justify-center flex-row gap-2"
+      >
         <span>{icon}</span>
         <span>{action}</span>
       </button>
     </div>
-  )
-}
+  );
+};
 
-export default Button
+export default Button;
--- src/pages/DealsPage.jsx
+++ src/pages/DealsPage.jsx
@@ -1,11 +1,60 @@
 import React from 'react';
 import Header from '../components/Header';
+import Button from '../components/Button'
+import TuneIcon from '@mui/icons-material/Tune';
+import Deal from '../components/Deal';
 
 const DealsPage = ({toggleDarkMode}) => {
   return (
     <div className="flex flex-col gap-3 bg-[#F9F9F9] dark:bg-[#0F1631] h-screen">
       <Header header="Deals" search="true" userProfile="false" toggleDarkMode={toggleDarkMode} />
-      <div className=''> hi</div>
+      <div className='flex flex-col mx-10 my-5 gap-7'>
+        <div>
+          <Button action={'Filter'} icon={<TuneIcon/>} bg={'#5547D7'} hoverBg={'#7094db'}/>
+        </div>
+        <div className='flex flex-row justify-between text-md font-semibold'>
+          <p>Name/Company</p>
+          <p>Deals</p>
+          <p>Value</p>
+          <p>Method</p>
+          <p>Status</p>
+          <p>Actions</p> 
+        </div>
+        <div className='flex flex-col gap-7'>
+          <Deal
+           image={'https://images.pexels.com/photos/1239291/pexels-photo-1239291.jpeg?auto=compress&cs=tinysrgb&w=600'}
+           name={'Kirezi Livia'}
+           detail1={'Application Deal'}
+           detail2={'Frontend Dev'}
+           price1={'10000'}
+           price2={'5000'}
+          />
+          <Deal
+           image={'https://images.pexels.com/photos/1222271/pexels-photo-1222271.jpeg?auto=compress&cs=tinysrgb&w=600'}
+           name={'Mack John'}
+           detail1={'Application Deal'}
+           detail2={'Backend Dev'}
+           price1={'10000'}
+           price2={'5000'}
+          />
+          <Deal
+           image={'https://images.pexels.com/photos/773371/pexels-photo-773371.jpeg?auto=compress&cs=tinysrgb&w=600'}
+           name={'Asifiwe Angele'}
+           detail1={'Work Deal'}
+           detail2={'Designing'}
+           price1={'10000'}
+           price2={'5000'}
+          />
+          <Deal
+           image={'https://images.pexels.com/photos/1239291/pexels-photo-1239291.jpeg?auto=compress&cs=tinysrgb&w=600'}
+           name={'Kirezi Livia'}
+           detail1={'Application Deal'}
+           detail2={'Frontend Dev'}
+           price1={'10000'}
+           price2={'5000'}
+          />
+        </div>
+      </div>
     </div>
   );
 };
